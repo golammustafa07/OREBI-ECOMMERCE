57a49800c35dbf761852eae0558dfe6a5d38e9cf
golammustafa07 <tapatap1234@gmail.com>
2025-02-16 19:00:47 +0600

    banner slider done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import Container from '../Layout/Container'
 import banner from '../../assets/banner.jpg'
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Banner = () => {
     return (
         <div className='max-w-containerx mx-auto'>
-            <img src={banner} alt="" />
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+                {
+                    Array.from({length:10}).map((_,i)=>{
+                        return(
+
+                <SwiperSlide key={i}>
+                    <img src={banner} alt="" />
+                </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
         </div>
     )
 }
